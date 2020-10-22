351e5b9e75345ce5f4f08dfe1345ef938656cdb3
danefalvo <danefalvo@gmail.com>
2020-10-22 22:44:21 +1100

    I don't know what I'm doing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyExamplePackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
